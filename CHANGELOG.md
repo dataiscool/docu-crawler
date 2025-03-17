856ce88a07337ccfa20f686431cd385dcef62c04
Fillipi Bittencourt <fillipi.bittencourt@AC-LPT-00597.local>
2025-03-17 09:49:28 -0300

    Fix GitHub Actions workflow and update Python version compatibility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [0.1.0] - 2025-03-17
+## [0.1.1] - 2025-03-17
 
 ### Added
 - Initial release
@@ -14,3 +14,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Local and Google Cloud Storage support
 - Command-line interface
 - Configuration file support
+
+### Changed
+- Updated Python compatibility to 3.9+ for modern environments
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="docu-crawler",
-    version="0.1.0",
+    version="0.1.1",
     description="Documentation Web Crawler that converts HTML to Markdown",
     author="Fillipi Bittencourt",
     author_email="fahbittencourt@gmail.com",
@@ -24,10 +24,10 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
